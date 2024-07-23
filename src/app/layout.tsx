ddc52bac79acab858338a201074ed5b88e0529fc
yunjiisy <dbswl330@gmail.com>
2024-07-23 14:49:53 +0900

    [Fix] 프리티어 오류 fix (문제 플러그인 제거)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,17 @@ import '../app/globals.css'
 
 import { ReactNode } from 'react'
 
-import NavBar from '@/components/common/NavBar';
+import NavBar from '@/components/common/NavBar'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-    <body className="flex justify-center items-center bg-white min-h-screen">
-    <div className=" w-[1440px] bg-white">
-      <NavBar />
-      <div className=' h-full'>
-        {children}
-      </div>
-    </div>
-    </body>
+    <html lang='en'>
+      <body className='flex min-h-screen items-center justify-center bg-white'>
+        <div className='w-[1440px] bg-white'>
+          <NavBar />
+          <div className='h-full'>{children}</div>
+        </div>
+      </body>
     </html>
-  );
+  )
 }
