fb7ff0eeb7cd9825e185b851f149571bbc5496e6
yunjiisy <dbswl330@gmail.com>
2024-07-23 11:00:07 +0900

    Fear: 상단 네비게이션 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,17 @@ import '../app/globals.css'
 
 import { ReactNode } from 'react'
 
+import NavBar from '@/components/common/NavBar';
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
     <body className="flex justify-center items-center bg-white min-h-screen">
-    <div className="w-[1440px] h-[1275px] bg-white">
-      {children}
+    <div className=" w-[1440px] bg-white">
+      <NavBar />
+      <div className=' h-full'>
+        {children}
+      </div>
     </div>
     </body>
     </html>
