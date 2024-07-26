b0e190af3b0c74b0504d40a730fbdcc1d35a6467
makisekurisu <kurikame1017@gmail.com>
2024-07-26 15:50:34 +0900

    [Feat] 시험 카드 api 연결

--- src/app/exam/page.tsx
+++ src/app/exam/page.tsx
@@ -17,12 +17,14 @@ export default function ExamPage(): JSX.Element {
   const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(true);
   const [filters, setFilters] = useState<Filters>({ startYear: 2021, endYear: 2023, months: [], types: [] });
+  const [searched, setSearched] = useState(false);
 
   const fetchProblems = async (newFilters: Filters, page: number = 0) => {
     if (page === 0) {
       setProblems([]);
     }
     setFilters(newFilters);
+    setSearched(true);
     try {
       const response = await fetch(`https://test.udongrang.com:7070/exam/search?page=${page}`, {
         method: 'POST',
@@ -51,7 +53,14 @@ export default function ExamPage(): JSX.Element {
         <FilterComponent onFilterChange={(newFilters) => fetchProblems(newFilters, 0)} />
       </div>
       <div className="w-full max-w-[1440px] bg-white p-4 mt-4 shadow-lg">
-        <ProblemCardList problems={problems} fetchMore={() => fetchProblems(filters, page)} hasMore={hasMore} />
+        {searched ? (
+          <ProblemCardList problems={problems} fetchMore={() => fetchProblems(filters, page)} hasMore={hasMore} />
+        ) : (
+          <div className="text-center text-gray-500">
+            <p>옵션을 선택해서 문제를 찾아보세요~
+              허전한가? 뭔 이미지 더 넣어야하나? 흠..</p>
+          </div>
+        )}
       </div>
     </div>
   );
