5a6faf055c6c885eaea570200d48281bf896b215
makisekurisu <kurikame1017@gmail.com>
2024-07-28 17:48:49 +0900

    [Feat] 유저프로필 api 연결, 상단 하단 나누기 (Css짜증나)

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,7 @@
-import { getUserProfile, UserProfile } from '@/api/profile/userProfile';
-import ProfileCard from '@/components/feature/profile/ProfileCard';
+import { getUserProfile, UserProfile } from '@/api/profile/UserProfile';
+import ActivityInfo from '@/components/feature/profile/ActivityInfo';
+import ProfileHeader from '@/components/feature/profile/ProfileHeader';
+import RecentAttempt from '@/components/feature/profile/RecentAttempt';
 
 export default async function ProfilePage() {
   const userId = 1; // 항상 ID 1을 사용
@@ -13,8 +15,14 @@ export default async function ProfilePage() {
   }
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      {user ? <ProfileCard user={user} /> : <div>Loading...</div>}
+    <div className="min-h-screen bg-gray-100 pt-20 flex flex-col items-center">
+      <div className="w-full max-w-screen-xl">
+        <ProfileHeader user={user} />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <ActivityInfo user={user} />
+          <RecentAttempt />
+        </div>
+      </div>
     </div>
   );
 }
