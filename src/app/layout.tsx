1f11665271d46a1b8d5485778021bc94b4491b55
yunjiisy <dbswl330@gmail.com>
2024-07-31 08:32:12 +0900

    Feat: 메인페이지 베너 구현 및 시험 페이지 스타일 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,12 @@ import NavBar from '@/components/common/NavBar'
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="flex justify-center items-center bg-white ">
-        <div className=" w-[80%] bg-white ">
-          <div className="w-[80%] fixed">
+      <body className="flex  justify-center items-center bg-white ">
+        <div className="w-[80%] bg-white ">
+          <div className=" z-20 bg-white w-[80%] fixed">
             <NavBar />
           </div>
-          <div className="pt-20">{children}</div>
+          <div className="w-full mt-20 border">{children}</div>
         </div>
       </body>
     </html>
--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -7,8 +7,8 @@ import { Button } from '../ui/button'
 
 export default function NavBar() {
   return (
-    <div className="z-20 border-b-2 w-full top-0 bg-white flex h-20 justify-between items-center border-brand-primary-100 px-4">
-      <div className="flex items-center text-brand-primary-500 text-base">
+    <div className="border-b-2 top-0 bg-white flex h-20 justify-between items-center border-brand-primary-100 px-4">
+      <div className="flex items-center text-brand-primary-500 text-base ">
         <div className="pr-10">
           <Math width="45" height="45" />
         </div>
