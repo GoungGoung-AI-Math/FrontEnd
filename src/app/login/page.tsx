3becd444bd5fc1f6d431329eb7f8643dda64df36
yunjiisy <dbswl330@gmail.com>
2024-07-30 09:16:03 +0900

    Merge

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,26 +1,34 @@
-import dynamic from 'next/dynamic';
+import dynamic from 'next/dynamic'
 
-import School from '@/assets/silverMedal.svg';
+import School from '@/assets/silverMedal.svg'
 
-const LoginButton = dynamic(() => import('../../components/feature/login/LoginButton'), { ssr: false });
+const LoginButton = dynamic(
+  () => import('../../components/feature/login/LoginButton'),
+  { ssr: false },
+)
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="flex flex-col md:flex-row w-full max-w-4xl bg-white shadow-lg">
-        <div className="w-full md:w-3/5 bg-brand-primary-500 p-10 flex flex-col justify-center items-center">
-          <School className="w-48 h-48 mb-8" />
-          <h2 className="text-white text-4xl mb-4">날씨가 습하네요</h2>
-          <p className="text-white text-lg mb-4">비오는 날</p>
-          <p className="text-white text-sm">
-            단지 내 쉼터를 점령<a href="#" className="underline"> 한곡</a> 시원하게 <a href="#" className="underline"></a>기관 절제술
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='flex flex-col md:flex-row w-full max-w-4xl bg-white shadow-lg'>
+        <div className='w-full md:w-3/5 bg-brand-primary-500 p-10 flex flex-col justify-center items-center'>
+          <School className='w-48 h-48 mb-8' />
+          <h2 className='text-white text-4xl mb-4'>날씨가 습하네요</h2>
+          <p className='text-white text-lg mb-4'>비오는 날</p>
+          <p className='text-white text-sm'>
+            단지 내 쉼터를 점령
+            <a href='#' className='underline'>
+              {' '}
+              한곡
+            </a>{' '}
+            시원하게 <a href='#' className='underline'></a>기관 절제술
           </p>
         </div>
-        <div className="w-full md:w-3/5 p-10">
-          <h2 className="text-2xl mb-6">로그인</h2>
+        <div className='w-full md:w-3/5 p-10'>
+          <h2 className='text-2xl mb-6'>로그인</h2>
           <LoginButton />
         </div>
       </div>
     </div>
-  );
+  )
 }
--- src/components/feature/exam/FilterComponent.tsx
+++ src/components/feature/exam/FilterComponent.tsx
@@ -1,40 +1,45 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/ui/button'
 
-import Label from '../../common/Label';
+import Label from '../../common/Label'
 
-const years = [2021, 2022, 2023];
-const months = ['전체', '03', '04', '06', '07', '09', '10', '11'];
-const types = ['전체', 'a', 'b', 'c'];
+const years = [2021, 2022, 2023]
+const months = ['전체', '03', '04', '06', '07', '09', '10', '11']
+const types = ['전체', 'a', 'b', 'c']
 
 interface FilterComponentProps {
-  onFilterChange: (filters: { startYear: number; endYear: number; months: string[]; types: string[] }) => void;
+  onFilterChange: (filters: {
+    startYear: number
+    endYear: number
+    months: string[]
+    types: string[]
+  }) => void
 }
 
 function FilterComponent({ onFilterChange }: FilterComponentProps) {
-  const [selectedStartYear, setSelectedStartYear] = useState<number>(2021);
-  const [selectedEndYear, setSelectedEndYear] = useState<number>(2023);
-  const [selectedMonths, setSelectedMonths] = useState<string[]>([]);
-  const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
+  const [selectedStartYear, setSelectedStartYear] = useState<number>(2021)
+  const [selectedEndYear, setSelectedEndYear] = useState<number>(2023)
+  const [selectedMonths, setSelectedMonths] = useState<string[]>([])
+  const [selectedTypes, setSelectedTypes] = useState<string[]>([])
 
   const toggleSelection = (
     item: string,
     setSelected: React.Dispatch<React.SetStateAction<string[]>>,
-    selectedItems: string[]
+    selectedItems: string[],
   ) => {
     if (item === '전체') {
-      setSelected(selectedItems.length === months.length ? [] : months);
+      setSelected(selectedItems.length === months.length ? [] : months)
     } else {
       if (selectedItems.includes(item)) {
-        setSelected(selectedItems.filter((i) => i !== item));
+        setSelected(selectedItems.filter((i) => i !== item))
       } else {
-        setSelected([...selectedItems, item]);
+        setSelected([...selectedItems, item])
       }
     }
-  };
+  }
 
   const handleButtonClick = () => {
     const filters = {
@@ -42,20 +47,20 @@ function FilterComponent({ onFilterChange }: FilterComponentProps) {
       endYear: selectedEndYear,
       months: selectedMonths.includes('전체') ? [] : selectedMonths,
       types: selectedTypes.includes('전체') ? [] : selectedTypes,
-    };
-    onFilterChange(filters);
-  };
+    }
+    onFilterChange(filters)
+  }
 
   return (
-    <div className="w-full p-4">
-      <div className="flex flex-col gap-6">
-        <div className="flex flex-row items-end gap-9 w-full">
-          <div className="flex flex-col">
-            <label className="font-bold text-[14px] text-[#1E1E1E]">연도</label>
+    <div className='w-full p-4'>
+      <div className='flex flex-col gap-6'>
+        <div className='flex flex-row items-end gap-9 w-full'>
+          <div className='flex flex-col'>
+            <label className='font-bold text-[14px] text-[#1E1E1E]'>연도</label>
             <select
               value={selectedStartYear}
               onChange={(e) => setSelectedStartYear(Number(e.target.value))}
-              className="mt-1 w-[120px] h-[40px] p-2 text-lg border border-[#D9D9D9] rounded-lg"
+              className='mt-1 w-[120px] h-[40px] p-2 text-lg border border-[#D9D9D9] rounded-lg'
             >
               {years.map((year) => (
                 <option key={year} value={year}>
@@ -64,12 +69,14 @@ function FilterComponent({ onFilterChange }: FilterComponentProps) {
               ))}
             </select>
           </div>
-          <div className="flex flex-col">
-            <label className="font-bold text-[14px] text-[#1E1E1E] invisible">연도</label>
+          <div className='flex flex-col'>
+            <label className='font-bold text-[14px] text-[#1E1E1E] invisible'>
+              연도
+            </label>
             <select
               value={selectedEndYear}
               onChange={(e) => setSelectedEndYear(Number(e.target.value))}
-              className="mt-1 w-[120px] h-[40px] p-2 text-lg border border-[#D9D9D9] rounded-lg"
+              className='mt-1 w-[120px] h-[40px] p-2 text-lg border border-[#D9D9D9] rounded-lg'
             >
               {years.map((year) => (
                 <option key={year} value={year}>
@@ -79,26 +86,38 @@ function FilterComponent({ onFilterChange }: FilterComponentProps) {
             </select>
           </div>
         </div>
-        <div className="flex flex-col w-full">
-          <label className="font-bold text-[14px] text-[#1E1E1E]">시험</label>
-          <div className="flex flex-wrap gap-4 mt-2">
+        <div className='flex flex-col w-full'>
+          <label className='font-bold text-[14px] text-[#1E1E1E]'>시험</label>
+          <div className='flex flex-wrap gap-4 mt-2'>
             {months.map((month) => (
-              <Label key={month} name={month} selectedValues={selectedMonths} toggleSelection={toggleSelection} setSelected={setSelectedMonths} />
+              <Label
+                key={month}
+                name={month}
+                selectedValues={selectedMonths}
+                toggleSelection={toggleSelection}
+                setSelected={setSelectedMonths}
+              />
             ))}
           </div>
         </div>
-        <div className="flex flex-col w-full">
-          <label className="font-bold text-[14px] text-[#1E1E1E]">영역</label>
-          <div className="flex flex-wrap gap-4 mt-2">
+        <div className='flex flex-col w-full'>
+          <label className='font-bold text-[14px] text-[#1E1E1E]'>영역</label>
+          <div className='flex flex-wrap gap-4 mt-2'>
             {types.map((type) => (
-              <Label key={type} name={type} selectedValues={selectedTypes} toggleSelection={toggleSelection} setSelected={setSelectedTypes} />
+              <Label
+                key={type}
+                name={type}
+                selectedValues={selectedTypes}
+                toggleSelection={toggleSelection}
+                setSelected={setSelectedTypes}
+              />
             ))}
           </div>
         </div>
-        <div className="flex justify-end">
+        <div className='flex justify-end'>
           <Button
-            variant="primary"
-            className="w-[54px] h-[40px] rounded-lg flex items-center justify-center cursor-pointer"
+            variant='primary'
+            className='w-[54px] h-[40px] rounded-lg flex items-center justify-center cursor-pointer'
             onClick={handleButtonClick}
           >
             확인
@@ -106,7 +125,7 @@ function FilterComponent({ onFilterChange }: FilterComponentProps) {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default FilterComponent;
+export default FilterComponent
