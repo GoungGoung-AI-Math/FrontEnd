5829990ca8055534259c68ae76eee87ab6d3dd09
yunjiisy <dbswl330@gmail.com>
2024-07-31 09:30:08 +0900

    Refactor: 네비게이팅 기능 추가

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 import Math from '@/assets/math_1411684.svg'
@@ -13,26 +14,24 @@ export default function NavBar() {
           <Math width="45" height="45" />
         </div>
         <Link className="px-4 py-1 rounded-md hover:bg-brand-primary-100" href={'/'}>
-          {' '}
           홈
         </Link>
-        <Link className="px-4 py-1 rounded-md hover:bg-brand-primary-100" href={'/'}>
-          {' '}
+        <Link className="px-4 py-1 rounded-md hover:bg-brand-primary-100" href={'/exam'}>
           교육청 기출
         </Link>
-        <Link className="px-4 py-1 rounded-md hover:bg-brand-primary-100" href={'/'}>
-          {' '}
+        <Link className="px-4 py-1 rounded-md hover:bg-brand-primary-100" href={'/exam'}>
           평가원 기출
         </Link>
         <Link className="px-4 py-1 rounded-md hover:bg-brand-primary-100" href={'/'}>
-          {' '}
           유형별 문제
         </Link>
       </div>
-
-      <Button variant={'primary'} className="w-24">
+      <Link
+        href={'/login'}
+        className=" bg-brand-primary-500 hover:bg-brand-primary-400 text-center text-white rounded-md w-24 flex items-center justify-center h-10"
+      >
         로그인
-      </Button>
+      </Link>
     </div>
   )
 }
